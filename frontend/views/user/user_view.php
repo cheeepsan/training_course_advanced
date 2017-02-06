e36f050a1ee6315c978daa194c5d911ed131671e
Chipson <chepsan@outlook.com>
2017-02-06 19:10:29 +0200

    some stuff

--- frontend/views/user/user_view.php
+++ frontend/views/user/user_view.php
@@ -8,42 +8,52 @@ use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use kartik\detail\DetailView;
 use yii\helpers\Url;
+
 $this->title = 'User view';
 
-$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="container">
-<div class="row">
-    <div class="col-md-8">
-        <h1><?= $model->name . ' ' . $model->surname ?></h1>
-        <?php $form = ActiveForm::begin([
-            'id' => 'add-course-form',
-            'options' => ['class' => '', 'enctype' => 'multipart/form-data'],
-            'fieldConfig' => [
-                //'template' => "{label}\n<div class=\"col-lg-7\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-                //'labelOptions' => ['class' => 'col-lg-1 control-label'],
-            ],
-        ]);
+<div clas="container">
+    <div class="row">
+        <div class="col-md-4 sidebar">
 
-        ?>
+            <ul>
+                <li>Basic info</li>
+                <li>Body</li>
+            </ul>
+        </div>
+        <div class="col-md-8">
+            <h1><?= $model->name . ' ' . $model->surname ?></h1>
+            <?php $form = ActiveForm::begin([
+                'id' => 'add-course-form',
+                'options' => ['class' => '', 'enctype' => 'multipart/form-data'],
+                'fieldConfig' => [
 
-        <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
-        <?= $form->field($model, 'surname')->textInput(['autofocus' => true]) ?>
+                ],
+            ]);
 
-        <?= $form->field($model, 'sex')->radioList([0 => 'Male', 1 => 'Female']) ?>
-        <?= $form->field($model, 'date_of_birth')->widget(yii\jui\DatePicker::classname(), ['dateFormat' => 'yyyy-MM-dd']) ?>
-        <?= $form->field($model, 'phone')->textInput(['autofocus' => true]) ?>
-        <?= $form->field($model, 'instagram')->textInput(['autofocus' => true]) ?>
-        <?= $form->field($model, 'facebook')->textInput(['autofocus' => true]) ?>
-        <div class="form-group">
-            <div class="col-lg-offset-1 col-lg-11">
-                <?= Html::submitButton('Create task', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+            ?>
+
+            <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
+            <?= $form->field($model, 'surname')->textInput(['autofocus' => true]) ?>
+
+            <?= $form->field($model, 'sex')->dropDownList([0 => 'Male', 1 => 'Female']) ?>
+            <?= $form->field($model, 'date_of_birth')->widget(yii\jui\DatePicker::classname(), ['dateFormat' => 'yyyy-MM-dd']) ?>
+            <?= $form->field($model, 'phone')->textInput(['autofocus' => true]) ?>
+            <?= $form->field($model, 'instagram')->textInput(['autofocus' => true]) ?>
+            <?= $form->field($model, 'facebook')->textInput(['autofocus' => true]) ?>
+            <div class="form-group">
+
+                <?= Html::submitButton('Save', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+
             </div>
+            <?php
+            $form::end();
+            ?>
+
         </div>
     </div>
 </div>
-</div>
 
-</div>
+
 
 
